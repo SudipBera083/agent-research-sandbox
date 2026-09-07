71c182816f49172877a55b968c86258033f22fa6
sudip <sudipbera083@gmail.com>
2026-09-07 23:03:58 +0530

    Updated the CORS policy

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ DEBUG = True
 ALLOWED_HOSTS = (
     os.getenv("ALLOWED_HOSTS", "").split(",")
     if os.getenv("ALLOWED_HOSTS")
-    else [".vercel.app", "localhost", "127.0.0.1"]
+    else [".vercel.app", "localhost", "127.0.0.1","*"]
 )
 
 # CORS: Allowed origins for Vercel frontend
